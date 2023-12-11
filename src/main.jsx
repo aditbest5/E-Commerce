add1a2531e6bc5034b160ac84ecaa16e0e20f25c
aditbest5 <aditbest5@gmail.com>
2023-12-11 13:08:29 +0700

    update

--- src/main.jsx
+++ src/main.jsx
@@ -6,9 +6,7 @@ import store from "./store";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
     <Provider store={store}>
       <App />
     </Provider>
-  </React.StrictMode>
 );
